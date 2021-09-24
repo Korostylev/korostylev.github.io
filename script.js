50f3843bd6e79f0f85a901de9b0876c0087fc95e
Korostylev <korostylevmaxim@mail.ru>
2021-09-24 15:45:23 +0300

    Шифровальщик с ключом

--- script.js
+++ script.js
@@ -1,51 +1,155 @@
-let center = [47.709237, 40.215401];
+const key = 12
+const strKey = 'pass'
 
-function init() {
-	let map = new ymaps.Map('map-test', {
-		center: center,
-		zoom: 15
-	});
+function dec2bin(dec){
+    return (dec>>> 0).toString(2);
+}
+function bin2dec(bin){
+    return parseInt(bin, 2).toString(10);
+}
+function xorForString(str){
+    const arr = str.split('')
+    // for (let i = 0; i < str.length; i++){
+    //     console.log(str[i])
+    // }
+    
+    // console.log(arr)
 
-    // Создание метки
-	let placemark1 = new ymaps.Placemark(center, {
-        hintContent: 'Собственный значок метки',
-        balloonContent: 'Это красивая метка<br><img src="http://mousosh5.ucoz.net/_tbk/tbh/sbory.jpeg" height="150" width="200">',
-    }, {
-		iconLayout: 'default#image',
-		iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
-		iconImageSize: [40, 40],
-		iconImageOffset: [-19, -44]
-	});
+    let newArr = []
 
-    // Метка с контентом
-    var placemark2 = new ymaps.Placemark([47.715900, 40.220953], {
-        // Зададим содержимое заголовка балуна.
-        balloonContentHeader: '<a href = "#">Преисподняя</a><br>' +
-            '<span class="description">Но это не точно</span>',
-        // Зададим содержимое основной части балуна.
-        balloonContentBody: 'А сюда можно html код. Ссылку на сайт, например <a href="http://mousosh5.ucoz.net/">5 школа</a><br>'+
-        'или картинку<br><img src="http://mousosh5.ucoz.net/_tbk/tbh/sbory.jpeg" height="150" width="200">',
-        // Зададим содержимое нижней части балуна.
-        balloonContentFooter: 'А это подпись',
-        // Зададим содержимое всплывающей подсказки.
-        hintContent: 'Адище'
-    });
+    for (let el of arr){
+        const code = el.charCodeAt()
+        // const bin = dec2bin(code)
+        const newSymbol = String.fromCharCode(code^key)
+        newArr.push(newSymbol)
+        console.log(el, code, newSymbol)
+    }
 
-	map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
-  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-
-  // Добавление метки
-	map.geoObjects.add(placemark1);
-    map.geoObjects.add(placemark2);
-
-    // Откроем балун на метке.
-    //placemark2.balloon.open();
+    console.log(newArr.join(''))
+    return newArr.join('')
 }
 
-ymaps.ready(init);
+function xorForStringKeyString(str, key){
+    const arr = str.split('')
+
+    const arrKey = key.split('')
+    const maxNumKey = arrKey.length
+    let idKey = 0
+
+    let newArr = []
+    for (const el of arr){
+        const code = el.charCodeAt()
+        const keyCode = arrKey[idKey].charCodeAt()
+
+        idKey++
+        if (idKey >= maxNumKey) idKey = 0
+
+        const newSymbol = String.fromCharCode(code^keyCode)
+        newArr.push(newSymbol)
+        // console.log(el, code, newSymbol)
+    }
+
+    // console.log(newArr.join(''))
+    return newArr.join('')
+}
+
+function downloadFile(filename, data) 
+{
+    var a = document.createElement('a');
+    a.style = "display: none";  
+    var blob = new Blob([data], {type: "application/octet-stream"});
+    var url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    window.setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url); 
+        }, 2000);
+}
+
+let num = 20
+let str = 'Hello World!'
+
+// console.log('num: ', num)
+// console.log('str: ', str)
+
+// console.log('dec2bin num: ', dec2bin(num))
+
+// let out = str.charCodeAt(0)
+// console.log(out)
+
+// xorForString(xorForString(str))
+
+// xorForStringKeyString(xorForStringKeyString(str, strKey), strKey)
+
+
+// document.querySelector('button').addEventListener('click', function() {
+//     const file = document.getElementById('file').files[0];
+//     const fileName = file.name;
+//     let reader = new FileReader();
+//     reader.readAsText(file);
+//     reader.onload = function() {
+//         const result = reader.result;
+//         // console.log(result);
+
+//         // const outputText = xorForStringKeyString(result, strKey)
+//         // // document.write('<br><a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(outputText) + '" download="' + fileName + '">Файл</a>');
+//         // downloadFile(fileName, outputText)
+//         downloadFile(fileName, result)
+//     }
+//     reader.onerror = function() {
+//       console.log(reader.error);
+//     }
+//   });
+
+function byteXOR(byteArrayData, byteArrayKey) {
+    let out = []
+    // for (const el of byteArrayData) {
+    //     out.push(el^key);
+    // }
+
+    // console.log(byteArrayData.length)
+
+    let keyID = 0;
+    const maxKeyID = byteArrayKey.length;
+
+    for (let i = 0; i < byteArrayData.length; i++){
+        out.push(byteArrayData[i] ^ byteArrayKey[keyID]);
+
+        keyID++;
+        if (keyID >= maxKeyID) keyID = 0;
+    }
+
+    return new Uint8Array(out);
+}
+
+document.querySelector('button').addEventListener('click', function() {
+    const fileDate = document.getElementById('fileDate').files[0];
+    const fileKey = document.getElementById('fileKey').files[0];
+    const fileName = fileDate.name;
+
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(fileDate);
+    let readerKey = new FileReader();
+    readerKey.readAsArrayBuffer(fileKey);
+
+
+    reader.onload = function() {
+        const result = reader.result;
+        const view = new Uint8Array(result);
+
+        const resultKey = readerKey.result;
+        const viewKey = new Uint8Array(resultKey);
+            
+        downloadFile(fileName, byteXOR(view, viewKey));
+    }
+    reader.onerror = function() {
+      console.log(reader.error);
+    }
+  });
+
+// Создание файла для скачивания
+// let text = 'как записать строку в файл ".txt" с помощью js?';
+// document.write('<br><a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(text) + '" download="text.txt">text.txt</a>');
