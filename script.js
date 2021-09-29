4adb2f0d888a3e6914f4557f2883286404fbd8b3
Korostylev <korostylevmaxim@mail.ru>
2021-09-29 12:55:30 +0300

    Много файлов, один ключ

--- script.js
+++ script.js
@@ -1,57 +1,7 @@
-const key = 12
-const strKey = 'pass'
-
-function dec2bin(dec){
-    return (dec>>> 0).toString(2);
-}
-function bin2dec(bin){
-    return parseInt(bin, 2).toString(10);
-}
-function xorForString(str){
-    const arr = str.split('')
-    // for (let i = 0; i < str.length; i++){
-    //     console.log(str[i])
-    // }
-    
-    // console.log(arr)
-
-    let newArr = []
-
-    for (let el of arr){
-        const code = el.charCodeAt()
-        // const bin = dec2bin(code)
-        const newSymbol = String.fromCharCode(code^key)
-        newArr.push(newSymbol)
-        console.log(el, code, newSymbol)
-    }
-
-    console.log(newArr.join(''))
-    return newArr.join('')
-}
-
-function xorForStringKeyString(str, key){
-    const arr = str.split('')
-
-    const arrKey = key.split('')
-    const maxNumKey = arrKey.length
-    let idKey = 0
-
-    let newArr = []
-    for (const el of arr){
-        const code = el.charCodeAt()
-        const keyCode = arrKey[idKey].charCodeAt()
-
-        idKey++
-        if (idKey >= maxNumKey) idKey = 0
-
-        const newSymbol = String.fromCharCode(code^keyCode)
-        newArr.push(newSymbol)
-        // console.log(el, code, newSymbol)
-    }
-
-    // console.log(newArr.join(''))
-    return newArr.join('')
-}
+const idDivMedia = "divOutMedia";
+const idDivListFiles = "divOutListFiles";
+const idFileKey = "fileKey";
+const idFiles = "multiFileDate";
 
 function downloadFile(filename, data) 
 {
@@ -69,49 +19,8 @@ function downloadFile(filename, data)
         }, 2000);
 }
 
-let num = 20
-let str = 'Hello World!'
-
-// console.log('num: ', num)
-// console.log('str: ', str)
-
-// console.log('dec2bin num: ', dec2bin(num))
-
-// let out = str.charCodeAt(0)
-// console.log(out)
-
-// xorForString(xorForString(str))
-
-// xorForStringKeyString(xorForStringKeyString(str, strKey), strKey)
-
-
-// document.querySelector('button').addEventListener('click', function() {
-//     const file = document.getElementById('file').files[0];
-//     const fileName = file.name;
-//     let reader = new FileReader();
-//     reader.readAsText(file);
-//     reader.onload = function() {
-//         const result = reader.result;
-//         // console.log(result);
-
-//         // const outputText = xorForStringKeyString(result, strKey)
-//         // // document.write('<br><a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(outputText) + '" download="' + fileName + '">Файл</a>');
-//         // downloadFile(fileName, outputText)
-//         downloadFile(fileName, result)
-//     }
-//     reader.onerror = function() {
-//       console.log(reader.error);
-//     }
-//   });
-
 function byteXOR(byteArrayData, byteArrayKey) {
     let out = []
-    // for (const el of byteArrayData) {
-    //     out.push(el^key);
-    // }
-
-    // console.log(byteArrayData.length)
-
     let keyID = 0;
     const maxKeyID = byteArrayKey.length;
 
@@ -125,31 +34,121 @@ function byteXOR(byteArrayData, byteArrayKey) {
     return new Uint8Array(out);
 }
 
-document.querySelector('button').addEventListener('click', function() {
-    const fileDate = document.getElementById('fileDate').files[0];
-    const fileKey = document.getElementById('fileKey').files[0];
-    const fileName = fileDate.name;
+function createText(text, containerId) {
+    let a = document.createElement('a');
+    a.innerHTML = text;
+    document.getElementById(containerId).appendChild(a);
+}
 
-    let reader = new FileReader();
-    reader.readAsArrayBuffer(fileDate);
-    let readerKey = new FileReader();
-    readerKey.readAsArrayBuffer(fileKey);
+function createDownloadButton(file, containerId) {
+    const name = file.name;
 
+    button = document.createElement("BUTTON");
+    buttonText = document.createTextNode("Скачать");
+    button.setAttribute("name", name);
+    button.appendChild(buttonText);
+    button.type = "button";
+    button.onclick = function() {
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(file);
 
-    reader.onload = function() {
-        const result = reader.result;
-        const view = new Uint8Array(result);
+        const fileKey = document.getElementById(idFileKey).files[0];
+        let readerKey = new FileReader();
+        readerKey.readAsArrayBuffer(fileKey);
 
-        const resultKey = readerKey.result;
-        const viewKey = new Uint8Array(resultKey);
+        reader.onload = function() {
+            const result = reader.result;
+            const view = new Uint8Array(result);
+
+            const resultKey = readerKey.result;
+            const viewKey = new Uint8Array(resultKey);
             
-        downloadFile(fileName, byteXOR(view, viewKey));
-    }
-    reader.onerror = function() {
-      console.log(reader.error);
-    }
-  });
+            console.log(view);
+            // console.log(viewKey);
+            // console.log(byteXOR(view, viewKey));
 
-// Создание файла для скачивания
-// let text = 'как записать строку в файл ".txt" с помощью js?';
-// document.write('<br><a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(text) + '" download="text.txt">text.txt</a>');
+            downloadFile(name, byteXOR(view, viewKey));
+        }
+    };
+    document.getElementById(containerId).appendChild(button);
+}
+function createOpenImageButton(file, containerId) {
+    const name = file.name;
+
+    button = document.createElement("BUTTON");
+    buttonText = document.createTextNode("Открыть как картинку");
+    button.setAttribute("name", name);
+    button.appendChild(buttonText);
+    button.type = "button";
+    button.onclick = function() {
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+
+        const fileKey = document.getElementById(idFileKey).files[0];
+        let readerKey = new FileReader();
+        readerKey.readAsArrayBuffer(fileKey);
+
+        reader.onload = function() {
+            const result = reader.result;
+            const view = new Uint8Array(result);
+
+            const resultKey = readerKey.result;
+            const viewKey = new Uint8Array(resultKey);
+
+            const file = byteXOR(view, viewKey);
+
+            let blob = new Blob([file], {type: "application/octet-stream"});
+            let url = window.URL.createObjectURL(blob);
+
+            const htmlCode = '<br><img src="' + url + '" alt="' + name + '" style="max-width:400px;width:100%">';
+            createText(htmlCode, idDivMedia);
+        }
+    };
+    document.getElementById(containerId).appendChild(button);
+}
+function createOpenVideoButton(file, containerId) {
+    const name = file.name;
+
+    button = document.createElement("BUTTON");
+    buttonText = document.createTextNode("Открыть как видео");
+    button.setAttribute("name", name);
+    button.appendChild(buttonText);
+    button.type = "button";
+    button.onclick = function() {
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+
+        const fileKey = document.getElementById(idFileKey).files[0];
+        let readerKey = new FileReader();
+        readerKey.readAsArrayBuffer(fileKey);
+
+        reader.onload = function() {
+            const result = reader.result;
+            const view = new Uint8Array(result);
+            
+            const resultKey = readerKey.result;
+            const viewKey = new Uint8Array(resultKey);
+
+            const file = byteXOR(view, viewKey);
+
+            let blob = new Blob([file], {type: "application/octet-stream"});
+            let url = window.URL.createObjectURL(blob);
+
+            const htmlCode = '<br><video src="' + url + '" controls="controls"></video>';
+            createText(htmlCode, idDivMedia);
+        }
+    };
+    document.getElementById(containerId).appendChild(button);
+}
+
+document.getElementById('btnEncryptor').addEventListener('click', function() {
+    const containerId = idDivListFiles;
+    const filesDate = document.getElementById(idFiles).files;
+
+    for (const el of filesDate) {        
+        createText('<br>' + el.name + '  :  ', containerId);
+        createDownloadButton(el, containerId);
+        createOpenImageButton(el, containerId);
+        createOpenVideoButton(el, containerId);
+    }
+});
